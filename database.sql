a9466de8d878b03853f10d96f981fef643fe61eb
Solange4 <sromeroc@unsa.edu.pe>
2020-08-16 23:26:36 -0500

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,2 @@
+CREATE TABLE student(course varchar(20),branch varchar(50),rollNo varchar(10) primary key,name varchar(100),fatherName varchar(100), gender varchar(10)) ;
+CREATE TABLE result (rollNo varchar(10) primary key,s1 int(3),s2 int(3),s3 int(3),s4 int(3),s5 int(3),s6 int(3));
